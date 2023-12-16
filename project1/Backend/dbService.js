39952a1b3a311e2b2a0bee72c77edf60ec8c1e77
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2023-12-16 15:22:28 -0500

    Update dbService.js

--- project1/Backend/dbService.js
+++ project1/Backend/dbService.js
@@ -35,7 +35,44 @@ class DbService{
         return instance? instance: new DbService();
     }
 
-    
+   /*
+     This code defines an asynchronous function getAllData using the async/await syntax. 
+     The purpose of this function is to retrieve all data from a database table named 
+     "names" using a SQL query.
+
+     Let's break down the code step by step:
+         - async getAllData() {: This line declares an asynchronous function named getAllData.
+
+         - try {: The try block is used to wrap the code that might throw an exception 
+            If any errors occur within the try block, they can be caught and handled in 
+            the catch block.
+
+         - const response = await new Promise((resolve, reject) => { ... });: 
+            This line uses the await keyword to pause the execution of the function 
+            until the Promise is resolved. Inside the await, there is a new Promise 
+            being created that represents the asynchronous operation of querying the 
+            database. resolve is called when the database query is successful, 
+            and it passes the query results. reject is called if there is an error 
+            during the query, and it passes an Error object with an error message.
+
+         - The connection.query method is used to execute the SQL query on the database.
+
+         - return response;: If the database query is successful, the function returns 
+           the response, which contains the results of the query.
+
+        - catch (error) {: The catch block is executed if an error occurs anywhere in 
+           the try block. It logs the error to the console.
+
+        - console.log(error);: This line logs the error to the console.   
+    }: Closes the catch block.
+
+    In summary, this function performs an asynchronous database query using await and a 
+   Promise to fetch all data from the "names" table. If the query is successful, 
+   it returns the results; otherwise, it catches and logs any errors that occur 
+   during the process. It's important to note that the await keyword is used here 
+   to work with the asynchronous nature of the connection.query method, allowing 
+   the function to pause until the query is completed.
+   */
     async getAllData(){
         try{
            // use await to call an asynchronous function
