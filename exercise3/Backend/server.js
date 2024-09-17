7af1c35f5e77eebb2de1bac7b01d2e281193a414
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-09-17 19:57:05 -0400

    Update server.js

--- exercise3/Backend/server.js
+++ exercise3/Backend/server.js
@@ -12,6 +12,48 @@ const dbconn = mysql.createConnection({
     database: "test"
 })
 
+const express = require('express');
+const mysql = require('mysql')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+})
+
+// let's see whether we can connect to the database successfully or not
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+
+app.get('/', (re, res) => {
+    return res.json("Welcome to the DB class.");
+})
+
+app.get('/listall', (request, response) => {
+    console.log("run sql now");
+    const sql = "SELECT * from students"
+    db.query(sql, (err, data) => {
+        if(err) return response.json(err);
+        else return response.json(data);
+    })
+ 
+})
+
+app.listen(8081, () => {
+    console.log("listening")
+})
+
 
 
 app.use(cors());
