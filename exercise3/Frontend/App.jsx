662813f02ad5b8a62ed087f37eac5d1ffdb01c14
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-09-17 22:30:15 -0400

    Update App.jsx

--- exercise3/Frontend/App.jsx
+++ exercise3/Frontend/App.jsx
@@ -1,15 +1,15 @@
 
-import React, {useEffect, useState} from 'react' // Imports the React library
+import React, {useEffect, useState} from 'react'    // Imports the React library
 
 function App() {
-    const [data, setData] = useState([]) //  Initializes a state variable called data and a function setData to update this state.
+    const [data, setData] = useState([])            //  Initializes a state variable called data and a function setData to update this state.
     
     useEffect(() => {                               // define the userEffect hook, useEffect(() => { ... }, [])
        fetch('http://localhost:8081/listall')       // call backend route
        .then(response => response.json())           // Converts the response from the fetch request into JSON format.
        .then(data => setData(data))                 // Updates the state variable data with the fetched data using the setData function.
-       .catch(err => console.log(err));
-    }, [])
+       .catch(err => console.log(err));             // logs the error msg to the console.
+    }, [])                                          // The empty dependency array [] means it only runs once when the component is first rendered.
 return(                                             // to be rendered in the UI
     <div>
        <table class="styled-table">
@@ -34,4 +34,4 @@ return(                                             // to be rendered in the UI
 )
 }
 
-export default App
+export default App                               // so it can be imported in other files.
