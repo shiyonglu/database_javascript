c76aa9959b39d02f610bad1939e655b984cc85e3
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2024-10-16 20:29:47 -0400

    Update app.js

--- project1/Backend/app.js
+++ project1/Backend/app.js
@@ -127,15 +127,19 @@ app.get('/testdb', (request, response) => {
 });
 
 
-  
-
-
-
-
-
 // set up the web server listener
+// if we use .env to configure
+/*
 app.listen(process.env.PORT, 
     () => {
-        console.log("I am listening.")
+        console.log("I am listening on the configured port " + process.env.PORT)
+    }
+);
+*/
+
+// if we configure here directly
+app.listen(5050, 
+    () => {
+        console.log("I am listening on the fixed port 5050.")
     }
 );
