994b0be5a8dcf957b3d466f7f5e985db855934ce
shiyonglu <32493145+shiyonglu@users.noreply.github.com>
2025-09-22 15:46:04 -0400

    Update dbService.js

--- project1/Backend/dbService.js
+++ project1/Backend/dbService.js
@@ -17,7 +17,7 @@ console.log("DB PORT: " + process.env.DB_PORT);
 
 const connection = mysql.createConnection({
      host: process.env.HOST,
-     user: process.env.USER,        
+     user: process.env.DB_USER,        
      password: process.env.PASSWORD,
      database: process.env.DATABASE,
      port: process.env.DB_PORT
